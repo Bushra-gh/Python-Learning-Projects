59a0c891be7a128a2d4e1815429a294b48393212
Bushra Rahman <rbushra402@gmail.com>
2026-06-22 17:33:45 +0530

    Replace random with secrets for password generation

--- PasswordGenerator/main.py
+++ PasswordGenerator/main.py
@@ -1,4 +1,4 @@
-import random
+import secrets
 
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 numbers = "0123456789"
@@ -20,21 +20,21 @@ else:
 def generate_password(length, include_letters, include_numbers, include_symbols):
     password = ""
     if include_letters.lower() == "y" and include_numbers.lower() == "y" and include_symbols.lower() == "y":
-        password = "".join(random.choice(all_characters) for _ in range(length))
+        password = "".join(secrets.choice(all_characters) for _ in range(length))
     elif include_letters.lower() == "y" and include_numbers.lower() == "y":
-        password = "".join(random.choice(no_symbols) for _ in range(length))
+        password = "".join(secrets.choice(no_symbols) for _ in range(length))
     elif include_letters.lower() == "y" and include_symbols.lower() == "y":
-        password = "".join(random.choice(no_numbers) for _ in range(length))
+        password = "".join(secrets.choice(no_numbers) for _ in range(length))
     elif include_numbers.lower() == "y" and include_symbols.lower() == "y":
-        password = "".join(random.choice(no_letters) for _ in range(length))
+        password = "".join(secrets.choice(no_letters) for _ in range(length))
     elif include_letters.lower() == "y":
-        password = "".join(random.choice(letters) for _ in range(length))
+        password = "".join(secrets.choice(letters) for _ in range(length))
     elif include_numbers.lower() == "y":
-        password = "".join(random.choice(numbers) for _ in range(length))
+        password = "".join(secrets.choice(numbers) for _ in range(length))
     elif include_symbols.lower() == "y":
-        password = "".join(random.choice(symbols) for _ in range(length))
+        password = "".join(secrets.choice(symbols) for _ in range(length))
     else:
-        return "invlalid request."
+        return "invalid request."
     return password
 
 generated_password = generate_password(length, include_letters, include_numbers, include_symbols)
